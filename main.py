b3730fa47fc94e33723263fb25ce04bf9ffcd4be
IciIcifur <iciicifur@yandex.ru>
2025-03-16 17:02:39 +0300

    Added custom CNN model
    - added simple CNN model
    - encapsulated extraction of features into a solid function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.preprocessing import collect_data, augment_normalize_data
 from src.resNet50 import extract_features_resnet50
+from src.cnn import extract_features_cnn
 from src.utils import check_folders, save_csv
 
 RAW_DATA_DIR = 'data/raw'
@@ -13,18 +14,33 @@ def main():
     # checking data folders existence
     check_folders(RAW_DATA_DIR, PROCESSED_DATA_DIR)
 
-    # load and augment dataset
-    data_paths, labels = collect_data(RAW_DATA_DIR)
-    train_data_generator, test_data_generator = augment_normalize_data(data_paths, labels)
+    print('\033[96mDo you need to extract features from dataset? y / n\033[0m')
+    if 'y' in input():
+        # load and augment dataset
+        data_paths, labels = collect_data(RAW_DATA_DIR)
+        train_data_generator, test_data_generator = augment_normalize_data(data_paths, labels)
 
-    # extract features from dataset via ResNet50
-    print('\n\033[92mExtracting from train:\033[0m')
-    X_train, y_train, train_df = extract_features_resnet50(train_data_generator)
-    save_csv(train_df, PROCESSED_DATA_DIR, 'resNet50_train.csv')
+        print('\033[96mResNet50? y / n\033[0m')
+        if 'y' in input():
+            # extract features from dataset via ResNet50
+            print('\n\033[92mExtracting from train:\033[0m')
+            X_train, y_train, train_df = extract_features_resnet50(train_data_generator)
+            save_csv(train_df, PROCESSED_DATA_DIR, 'resNet50_train.csv')
 
-    print('\n\033[92mExtracting from test:\033[0m')
-    X_test, y_test, test_df = extract_features_resnet50(test_data_generator)
-    save_csv(test_df, PROCESSED_DATA_DIR, 'resNet50_test.csv')
+            print('\n\033[92mExtracting from test:\033[0m')
+            X_test, y_test, test_df = extract_features_resnet50(test_data_generator)
+            save_csv(test_df, PROCESSED_DATA_DIR, 'resNet50_test.csv')
+
+        print('\033[96mCustom CNN? y / n\033[0m')
+        if 'y' in input():
+            # extract features from dataset via custom CNN
+            print('\n\033[92mExtracting from train:\033[0m')
+            X_train, y_train, train_df = extract_features_cnn(train_data_generator, RAW_DATA_DIR)
+            save_csv(train_df, PROCESSED_DATA_DIR, 'cnn_train.csv')
+
+            print('\n\033[92mExtracting from test:\033[0m')
+            X_test, y_test, test_df = extract_features_cnn(test_data_generator, RAW_DATA_DIR)
+            save_csv(test_df, PROCESSED_DATA_DIR, 'cnn_test.csv')
 
     # train models
 
--- src/resNet50.py
+++ src/resNet50.py
@@ -1,9 +1,7 @@
-from time import time
-import numpy as np
-import pandas as pd
 from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.models import Model
+
+from src.utils import extract_features
 
 
 def extract_features_resnet50(data_generator):
@@ -11,37 +9,4 @@ def extract_features_resnet50(data_generator):
     base_model = ResNet50(weights='imagenet', include_top=False, pooling='avg')  # pretrained model
     model = Model(inputs=base_model.input, outputs=base_model.output)
 
-    batches_count = len(data_generator)
-    i = 0
-    print(f"\033[93m{batches_count} batches is going to be processed.\033[0m")
-
-    # Extracting features:
-    features = []
-    labels = []
-
-    start_time = time()
-    for batch in data_generator:
-        X, y = batch
-        X = preprocess_input(X)
-        feature = model.predict(X)
-        features.append(feature)
-        labels.append(y)
-
-        i += 1
-        if i % 5 == 0:
-            print(f"\033[93mProcessed {int(i / batches_count * 100)}%: {i}/{batches_count}\033[0m")
-        if i == batches_count:
-            break
-    end_time = time()
-
-    print(f"\033[96mProcessed in {(end_time - start_time) / 60:.2f} minutes\033[0m")
-
-    X = np.concatenate(features, axis=0)
-    y = np.concatenate([np.argmax(batch, axis=1) for batch in labels], axis=0)
-
-    # Transforming 4D features into 2D array
-    X_reshaped = X.reshape(X.shape[0], -1)
-
-    joint_df = pd.DataFrame(X_reshaped)
-    joint_df['flower_type'] = y
-    return X, y, joint_df
+    return extract_features(data_generator, model, resnet50=True)
