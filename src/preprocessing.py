8fe9f642f023913fb066262746fb35a132f83aa1
IciIcifur <iciicifur@yandex.ru>
2025-03-18 10:54:42 +0300

    Added paddings to images via resizing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,11 @@
 import os
-import pandas as pd
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+import pandas as pd
+from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img
+
+
+# python -c "import PIL;"
 
 def save_paths(target_dir):
     paths = []
@@ -27,9 +31,25 @@ def collect_data(raw_data_dir):
     return data, labels
 
 
+def add_padding(image, target_size):
+    old_size = image.size
+    ratio = float(target_size[0]) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+    image = image.resize(new_size, Image.LANCZOS)
+    new_image = Image.new("RGB", target_size)
+    new_image.paste(image, ((target_size[0] - new_size[0]) // 2, (target_size[1] - new_size[1]) // 2))
+    return new_image
+
+
 def augment_normalize_data(data_paths, labels, batch_size=64, target_size=(224, 224)):
+    def preprocessing_function(image):
+        image = array_to_img(image)
+        image = add_padding(image, target_size)
+        return img_to_array(image)
+
     # train might be normalized and augmented
     train_transformations = ImageDataGenerator(
+        preprocessing_function=preprocessing_function,
         rescale=1. / 255,  # Normalization
         rotation_range=20,
         zoom_range=0.2,
@@ -39,7 +59,7 @@ def augment_normalize_data(data_paths, labels, batch_size=64, target_size=(224, 
         fill_mode='nearest')
 
     # test can be normalized only
-    test_transformations = ImageDataGenerator(rescale=1. / 255)
+    test_transformations = ImageDataGenerator(preprocessing_function=preprocessing_function, rescale=1. / 255)
 
     train_generator = train_transformations.flow_from_dataframe(
         dataframe=pd.DataFrame({'filename': data_paths['train'], 'class': labels['train']}),
